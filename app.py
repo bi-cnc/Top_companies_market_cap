e6a6a85dc75ae386f91932675deb91914592e946
bi-cnc <137881662+bi-cnc@users.noreply.github.com>
2023-10-17 16:51:36 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 import streamlit as st
 import pandas as pd
 import os
@@ -13,8 +14,6 @@ st.markdown("""
 </style>
 """, unsafe_allow_html=True)
 
-
-
 # Nahrání dat
 @st.cache_data
 def load_data():
@@ -29,10 +28,10 @@ st.write("")
 
 output_dir = 'downloaded_logos'
 
-# Vytvoření hlavičky tabulky s menšími nadpisy
-col_headers = ['\u200B', '\u200B', 'Název společnosti', 'Tržní kapitalizace (v mld. USD)', 'Cena za 1 akcii (USD)']
-col1, col_logo, col2, col3, col4 = st.columns([1, 1, 7, 7, 5])
-columns = [col1, col_logo, col2, col3, col4]
+# Vytvoření hlavičky tabulky s menšími nadpisy (Odstraněný sloupec 'Cena za 1 akcii (USD)')
+col_headers = ['\u200B', '\u200B', 'Název společnosti', 'Tržní kapitalizace (v mld. USD)']
+col1, col_logo, col2, col3 = st.columns([1, 1, 4, 3])  # Změněna distribuce sloupců
+columns = [col1, col_logo, col2, col3]
 for col, header in zip(columns, col_headers):
     col.markdown(f"<h3 style='text-align: center; font-size: 16px;'>{header}</h3>", unsafe_allow_html=True)
 
@@ -59,20 +58,16 @@ for index, row in data.head(25).iterrows():
     
     # Název společnosti se zvětšeným písmem
     with col2:
-        st.markdown(f"<div style='text-align: center; line-height: 40px; margin-bottom: 20px; font-size: 18px;'>{company_name}</div>", unsafe_allow_html=True)
+        st.markdown(f"<div style='text-align: center; line-height: 40px; margin-bottom: 20px; font-size: 16px;'>{company_name}</div>", unsafe_allow_html=True)
     
     # Tržní kapitalizace
     with col3:
         market_cap_value = str(row['Market Cap'])
         st.markdown(f"<div style='text-align: center; margin-bottom: 20px; line-height: 40px;'>{market_cap_value}</div>", unsafe_allow_html=True)
 
-
-    # Uzavírací cena akcie za poslední den
-    with col4:
-        st.markdown(f"<div style='text-align: center; margin-bottom: 20px; line-height: 40px;'>{row['Price']}</div>", unsafe_allow_html=True)
-
 # ...
 
 st.markdown('---')  # Toto vytvoří horizontální čáru pro oddělení obsahu
 st.markdown('**Zdroj:** companiesmarketcap.com | **Autor:** lig')
+
 
